fa9c7eedc5f8285655ebf16d6f5949ebca4d8e49
Charlie <charles.firth@cognitoiq.com>
2017-02-21 18:46:17 +0000

    Added Cassandra connection

--- src/main/java/Storm/Util/SequencingBolt.java
+++ src/main/java/Storm/Util/SequencingBolt.java
@@ -2,6 +2,9 @@ package Storm.Util;
 
 import Storm.AMQPHandler.JSONObjects.ItemState;
 import Storm.DatabaseHandler.LookupHandler;
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.IRichBolt;
@@ -9,7 +12,9 @@ import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -32,6 +37,21 @@ public class SequencingBolt implements IRichBolt {
         switch (tuple.getSourceStreamId()) {
             case "item":
                 /* Goto Cassandra. Check for item states. Emit them */
+
+                Cluster cassandraCluster = null;
+                HashMap<String, String[]> messagesInCassandra = new HashMap<>();
+
+                cassandraCluster = Cluster.builder()
+                        .addContactPoints(PropertiesHolder.getValue("cassandra_urls").split(","))
+                        .withClusterName("foo")
+                        .withPort(9042)
+                        .withCredentials("guest", "guest")
+                        .build();
+
+                Session session = cassandraCluster.connect();
+                ResultSet resultSet = session.execute("SELECT aggregateid, eventname FROM cqrs.event");
+
+
                 List<ItemState> states = new ArrayList<>();
                 for (ItemState state : states)
                     _collector.emit("item-state", tuple, new Values(state));
